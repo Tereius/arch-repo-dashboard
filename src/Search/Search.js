068d696c2fd24c9a76a39e024d61630ee97d8e5b
Björn Stresing <bjoern.stresing@gmx.de>
2022-05-28 17:07:22 +0200

    Improve search

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,44 +1,51 @@
 import Fuse from 'fuse.js';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef, useMemo } from 'react';
 import { useRepoDb } from '../hooks/useRepoDb';
-import { Dropdown, NavItem, NavLink } from 'react-bootstrap';
+import { Dropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 export function Search() {
   const data = useRepoDb();
   const [searchText, setSearchText] = useState('');
+  const [show, setShow] = React.useState(false);
   const [focused, setFocused] = React.useState(false);
   const [searchResult, setSearchResult] = useState([]);
 
   const onFocus = () => setFocused(true);
   const onBlur = () => setFocused(false);
 
-  const options = {
-    includeScore: true,
-    // Search in `author` and in `tags` array
-    keys: [
-      {
-        name: 'name',
-      },
-    ],
-  };
-
-  let fuse = new Fuse(Array.from(data.values()), options);
-
-  useEffect(() => {
-    fuse = new Fuse(Array.from(data.values()), options);
+  let fuse = useMemo(() => {
+    return new Fuse(Array.from(data.values()), {
+      includeScore: true,
+      keys: [
+        {
+          name: 'name',
+        },
+      ],
+    });
   }, [data]);
 
   useEffect(() => {
     const result = fuse.search(searchText);
     setSearchResult(result.slice(0, Math.min(20, result.length)));
-  }, [searchText]);
+  }, [searchText, fuse]);
 
-  const CustomToggle = React.forwardRef(({ children, onClick }, ref) => <span>{children}</span>);
+  useEffect(() => {
+    if (searchResult.length > 0) {
+      setShow(focused);
+    } else {
+      setShow(false);
+    }
+  }, [searchResult, focused]);
+
+  const inputRef = useRef();
+  const onSelected = (key, event) => {
+    inputRef.current.blur();
+  };
 
   return (
     <>
-      <Dropdown autoClose="true" show={focused}>
+      <Dropdown autoClose="true" onSelect={onSelected} show={show}>
         <Dropdown.Toggle as={'form'} split={true} bsPrefix="ignore">
           <input
             className="form-control me-2"
@@ -48,11 +55,12 @@ export function Search() {
             placeholder="Search"
             aria-label="Search"
             onChange={event => setSearchText(event.target.value)}
+            ref={inputRef}
           />
         </Dropdown.Toggle>
         <Dropdown.Menu>
           {searchResult.map(item => (
-            <Dropdown.Item key={item.item.name} href={'/package/' + item.item.name}>
+            <Dropdown.Item as={Link} key={item.item.name} to={'/package/' + item.item.name} onMouseDown={event => event.preventDefault()}>
               {item.item.name}
             </Dropdown.Item>
           ))}
