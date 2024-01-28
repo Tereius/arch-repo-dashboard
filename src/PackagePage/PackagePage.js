2c70d9c41285776f78aa0b4066b00e2bb461011c
Björn Stresing <bjoern.stresing@gmx.de>
2024-01-28 22:55:37 +0100

    Fix 404 page "Go back" link

--- src/PackagePage/PackagePage.js
+++ src/PackagePage/PackagePage.js
@@ -23,14 +23,10 @@ export function PackagePage() {
       <div class="d-flex align-items-center justify-content-center vh-100">
         <div class="text-center">
           <h1 class="display-1 fw-bold">404</h1>
-          <p class="fs-3">
-            {' '}
-            <span class="text-danger">Opps!</span> Package not found.
-          </p>
-          <p class="lead">The page you’re looking for doesn’t exist.</p>
-          <a href="/packages" class="btn btn-primary">
-            Go Home
-          </a>
+          <p class="lead">The package you’re looking for doesn’t exist.</p>
+          <p><Link to="/packages">Go back</Link></p>
+          <p><a href={"https://archlinux.org/packages/?sort=&q=" + params.packageName} target="_blank" rel="noopener noreferrer">Search "{params.packageName}" Arch package</a></p>
+          <p><a href={"https://aur.archlinux.org/packages?O=0&K=" + params.packageName} target="_blank" rel="noopener noreferrer">Search "{params.packageName}" Aur package</a></p>
         </div>
       </div>
     );
